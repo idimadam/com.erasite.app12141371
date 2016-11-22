10fe67a517a690d602fbdbefeecf62008816ac18
idimadam <imamab7@gmail.com>
2016-11-22 16:15:44 +0700

    12141371 - com.erasite.app12141371 adalah nama dari package yang saya buat. IMPORT merupakan kata kunci untuk melibatkan class class lain yang terhimpun  di dalam package. GameTebakAngka adalah nama class yang telah dibuat.

---
+++ src/com/erasite/app12141371/GameTebakAngka.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.erasite.app12141371;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * 
+ * @author IDIMADAM
+ */
+public class GameTebakAngka {
